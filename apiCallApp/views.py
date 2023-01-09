b7527209b58f25ba3c48cb6611a2b008e6ad6de8
Kalidas <k.s.kalidasraja@gmail.com>
2023-01-09 11:43:51 +0530

    Initial commit

---
+++ apiCallApp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import requests
+
+
+def user(request):
+    headers = {
+        "Authorization" : "Bearer 5isV+muNV9NH4aIuDf7Y01L16gBn9CiXRBpqkxibfigtYTmqYZjVxSwXzGllc7rao75T+fEbnnCtHK7/ZfUT8g==RmHrpq3beUipI3RlSVPRxQ==",
+        "Content-Type" : "application/json"
+    }
+    body ={
+        "storageTypeId": [
+            '3353a653-f8c2-411b-a6d0-bc6b9e20c8fd'
+        ],
+        "locationId": [],
+        "buildingId": []
+    }
+    response = requests.post('https://api.8storage.com/v1/unit/storagetype/', headers = headers, json = body)
+    data = response.json()
+    print(data)
+    print(response.status_code)
+
+    return JsonResponse(data, safe=False)
+    pass
